302e847b789ad60c6f8dcc227ebc9967834eb2ad
Mr-Rahul-Paul <179798584+Mr-Rahul-Paul@users.noreply.github.com>
2025-08-17 15:06:10 +0530

    prints file meta data now

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use clap::Parser;
 use owo_colors::OwoColorize;
 use std::{
-    fs,
+    fs::{self},
+    // fs::{self, File},
     path::{Path, PathBuf},
 };
+use strum::Display;
 
 #[derive(Debug, Parser)]
 #[command(/*-V*/version = "1.0", author = "Paul", /*-h and --help*/about="testing",long_about = "A simple CLI tool")]
@@ -11,14 +13,26 @@ struct Cli {
     path: Option<PathBuf>,
 }
 
+#[derive(Debug, Display)]
+enum EntryType {
+    File,
+    Dir,
+}
+#[derive(Debug)]
+struct FileEntry {
+    name: String,
+    e_type: EntryType,
+    len_bytes: u64,
+    modified: String,
+}
 fn main() {
     let cli = Cli::parse();
     let path = cli.path.unwrap_or(PathBuf::from("."));
 
     if let Ok(does_exist) = fs::exists(&path) {
         if does_exist {
-            for file in get_files(& path){
-                println!("{}",file);
+            for file in get_files(&path) {
+                println!("{:?}", file);
             }
         } else {
             println!("{}", "path doesnt exist".red());
@@ -31,18 +45,28 @@ fn main() {
     print!("working?? path info -- {:?}\n", path);
 }
 
-fn get_files(path: &Path) -> Vec<String> {
+fn get_files(path: &Path) -> Vec<FileEntry> {
     let mut data = Vec::default();
     if let Ok(read_dir) = fs::read_dir(path) {
         for entry in read_dir {
             if let Ok(file) = entry {
-                data.push(
-                    file.file_name()
-                        .into_string()
-                        .unwrap_or("unknown name".into()),
-                );
+                if let Ok(meta) = fs::metadata(&file.path()) {
+                    data.push(FileEntry {
+                        name: file
+                            .file_name()
+                            .into_string()
+                            .unwrap_or("unknown name".into()),
+                        e_type: if meta.is_dir() {
+                            EntryType::Dir
+                        } else {
+                            EntryType::File
+                        },
+                        len_bytes: meta.len(),
+                        modified: "".to_string(), // placeholder for now
+                    });
+                }
             }
         }
     }
-    return data;
+    data
 }
