a5699d2f8b65475e2794513dbad2579f6bd741ef
Mr-Rahul-Paul <179798584+Mr-Rahul-Paul@users.noreply.github.com>
2025-08-17 14:32:58 +0530

    now i can print files

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,48 @@
-use clap::{Arg, ArgGroup, command};
+use clap::Parser;
+use owo_colors::OwoColorize;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+#[derive(Debug, Parser)]
+#[command(/*-V*/version = "1.0", author = "Paul", /*-h and --help*/about="testing",long_about = "A simple CLI tool")]
+struct Cli {
+    path: Option<PathBuf>,
+}
 
 fn main() {
-    let match_result = command!()
-        .about("this is a test program")
-        .group(ArgGroup::new("person-register")
-        .args(["firstname", "lastname"])) 
-        .arg(
-            Arg::new("firstname")
-                .short('f')
-                .long("first-name")
-                .aliases(["fname", "first"]) //hidden alias
-                .required(true), // make the argument required
-        )
-        .arg(
-            Arg::new("lastname")
-                .short('l')
-                .long("last-name")
-                .aliases(["lname", "lastname"]),
-        )
-        .get_matches();
+    let cli = Cli::parse();
+    let path = cli.path.unwrap_or(PathBuf::from("."));
 
-    print!(
-        "firstname: {:?}\n",
-        match_result.get_one::<String>("firstname")
-    );
-    print!(
-        "lastname: {:?}\n",
-        match_result.get_one::<String>("lastname")
-    );
+    if let Ok(does_exist) = fs::exists(&path) {
+        if does_exist {
+            for file in get_files(& path){
+                println!("{}",file);
+            }
+        } else {
+            println!("{}", "path doesnt exist".red());
+        }
+    } else {
+        println!("{}", "error checking path".red());
+        // return;
+    }
+
+    print!("working?? path info -- {:?}\n", path);
+}
+
+fn get_files(path: &Path) -> Vec<String> {
+    let mut data = Vec::default();
+    if let Ok(read_dir) = fs::read_dir(path) {
+        for entry in read_dir {
+            if let Ok(file) = entry {
+                data.push(
+                    file.file_name()
+                        .into_string()
+                        .unwrap_or("unknown name".into()),
+                );
+            }
+        }
+    }
+    return data;
 }
