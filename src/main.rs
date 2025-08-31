20fcfaac597bc205270e5c1d9ddfb6f84ea6f10c
Mr-Rahul-Paul <179798584+Mr-Rahul-Paul@users.noreply.github.com>
2025-08-31 19:35:13 +0530

    added case insensitivity

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ struct Cli {
     /// The pattern to search for
     pattern: String,
     path: PathBuf,
+
+    #[arg(short = 'i', long = "ignore-case", default_value = "false")]
+    ignore_case: bool,
 }
 fn main() -> Result<()> {
     let args = Cli::parse();
@@ -17,23 +20,36 @@ fn main() -> Result<()> {
     let content = fs::read_to_string(&args.path)
         .with_context(|| format!("Could not read file `{:?}`", args.path))?;
 
-    find_matches(&content, &args.pattern);
+    find_matches(&content, &args.pattern, args.ignore_case);
 
     Ok(())
     // dont use " Ok(()); "
 }
 
-fn find_matches(content: &str, pattern: &str) {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            //color the output
-            // Split the line by the pattern
-            let parts: Vec<&str> = line.split(pattern).collect();
-            
-          
-            let colored = parts.join(&pattern.red().bold().to_string());
+fn find_matches(content: &str, pattern: &str, ignore_case: bool) {
+    if ignore_case {
+        let lower_pattern = pattern.to_lowercase();
+        for line in content.lines() {
+            // The .find() method returns an Option<usize> containing the byte index where the pattern begins.
+            if let Some(start_index) = line.to_lowercase().find(&lower_pattern) {
+                let end_index = start_index + pattern.len();
+                let before = &line[..start_index];
+                let matched_part = &line[start_index..end_index];
+                let after = &line[end_index..];
+                println!("{}{}{}", before, matched_part.red().bold(), after);
+            }
+        }
+    } else {
+        for line in content.lines() {
+            if line.contains(pattern) {
+                //color the output
+                // Split the line by the pattern
+                let parts: Vec<&str> = line.split(pattern).collect();
 
-            println!("{}", colored);
+                let colored = parts.join(&pattern.red().bold().to_string());
+
+                println!("{}", colored);
+            }
         }
     }
 }
