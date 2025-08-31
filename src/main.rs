17b7db1e628ed9ea1606166436747c507e558ee1
Mr-Rahul-Paul <179798584+Mr-Rahul-Paul@users.noreply.github.com>
2025-08-31 16:21:24 +0530

    now it reads the file and prints the text inside

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+use anyhow::{Context, Result};
 use clap::Parser;
-use std::path::PathBuf;
-#[derive(Parser)]
-#[command(version = "1.0" , author = "Paul")]
-struct Cli {
-    pattern : String,  /// The pattern to search for
-    path : PathBuf,
-}
-fn main(){
+use std::{fs, path::PathBuf};
 
+#[derive(Parser)]
+#[command(version = "1.0", author = "Paul")]
+struct Cli {
+    /* this under can be used to describe when pattern when cr -- --help */
+    /// The pattern to search for
+    pattern: String,
+    path: PathBuf,
+}
+fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    let content = fs::read_to_string(&args.path)
+        .with_context(|| format!("Could not read file `{:?}`", args.path))?;
+
+    println!("{}", content);
+
+    Ok(())
+    // dont use " Ok(()); "
 }
